6855d85c19355d4becf2d47f4d381d95b6fdfcc2
Andie <zchangan@163.com>
2017-10-31 18:36:25 +0800

    cancel

--- cmd/cancel.go
+++ cmd/cancel.go
@@ -1,8 +1,11 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 
+	"github.com/Andiedie/agenda-go/service"
+	"github.com/Andiedie/agenda-go/tools"
 	"github.com/spf13/cobra"
 )
 
@@ -12,7 +15,15 @@ var cancelCmd = &cobra.Command{
 	Short: "cancel a meeting",
 	Long:  `cancel a meeting`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("cancel called")
+		title, _ := cmd.Flags().GetString("title")
+		if title == "" {
+			tools.Report(errors.New("title required"))
+		}
+		if err := service.DeleteMeeting(title); err == nil {
+			fmt.Println("Success")
+		} else {
+			tools.Report(err)
+		}
 	},
 }
 
