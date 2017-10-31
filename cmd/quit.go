ac832eb05ed7a1830d7ac344a60486da1929ce5b
Andie <zchangan@163.com>
2017-10-31 19:10:51 +0800

    quit

--- cmd/quit.go
+++ cmd/quit.go
@@ -1,8 +1,11 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 
+	"github.com/Andiedie/agenda-go/service"
+	"github.com/Andiedie/agenda-go/tools"
 	"github.com/spf13/cobra"
 )
 
@@ -12,7 +15,16 @@ var quitCmd = &cobra.Command{
 	Short: "quit a meeting",
 	Long:  `quit a meeting`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("quit called")
+		title, _ := cmd.Flags().GetString("title")
+		if title == "" {
+			tools.Report(errors.New("title required"))
+		}
+		err := service.QuitMeeting(title)
+		if err == nil {
+			fmt.Println("Success")
+		} else {
+			tools.Report(err)
+		}
 	},
 }
 
