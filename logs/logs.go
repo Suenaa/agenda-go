fc5eea825b5ffb5c53a10e56cf9fb0e3c83140a4
Suenaa <1165153863@qq.com>
2017-11-01 13:37:38 +0800

    update log

--- logs/logs.go
+++ logs/logs.go
@@ -9,13 +9,23 @@ import (
 var (
 	out, e = os.OpenFile("logs/logs.log", os.O_APPEND | os.O_WRONLY | os.O_CREATE, 0666)
 	outWriter = io.Writer(out)
-	logger = log.New(outWriter, "[error] ", log.LstdFlags)
+	logger = log.New(outWriter, "[agenda] ", log.LstdFlags)
 )
 
-func Log(err error) {
+func ErrLog(err error) {
 	if e != nil {
 		log.Fatalln("log file error")
 	}
 	defer out.Close()
+	logger.SetPrefix("[error]")
 	logger.Println(err)
+}
+
+func EventLog(op string) {
+	if e != nil {
+		log.Fatalln("log file error")
+	}
+	defer out.Close()
+	logger.SetPrefix("[event]")
+	logger.Println(op)
 }
