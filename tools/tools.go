8da1e8a4949db1fde033cc34af3c483eeb9cf6d4
Suenaa <1165153863@qq.com>
2017-11-01 12:29:34 +0800

    add log

--- tools/tools.go
+++ tools/tools.go
@@ -3,12 +3,14 @@ package tools
 import (
 	"fmt"
 	"os"
+	logs "github.com/Suenaa/agenda-go/logs"
 )
 
 // Report 输出错误
 func Report(err error) {
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
+		logs.Log(err)
 		os.Exit(2)
 	}
 }
